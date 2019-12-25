9eb7f5cdb3eca45ebbe6abe9f52583b99f84ef0a
Ahmet Selçuk Özdemir <ahmetselcukozdemir01@gmail.com>
2019-12-25 17:45:22 +0300

    commit1

---
+++ Mvc/Controllers/EmployeeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mvc.Models;
+using System.Net.Http;
+
+namespace Mvc.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            IEnumerable<MvcEmployeeModel> empList;
+            HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Employee").Result;
+            empList = response.Content.ReadAsAsync<IEnumerable<MvcEmployeeModel>>().Result;
+            return View(empList);
+        }
+
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            if (id==0)
+           
+            return View(new MvcEmployeeModel());
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Employee/"+id.ToString()).Result;
+                return View(response.Content.ReadAsAsync<MvcEmployeeModel>().Result);
+            }
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit(MvcEmployeeModel emp)
+        {
+            if (emp.EmployeeID == 0) { 
+            HttpResponseMessage response = GlobalVariables.WebApiClient.PostAsJsonAsync("Employee", emp).Result;
+            TempData["SuccessMessage"] = "Saved succesfully";
+        
+            }
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PutAsJsonAsync("Employee/"+emp.EmployeeID,emp).Result;
+                TempData["SuccessMessage"] = "Updated succesfully";
+            }
+            return RedirectToAction("Index");
+
+        }
+        public ActionResult Delete(int id)
+        {
+            HttpResponseMessage response = GlobalVariables.WebApiClient.DeleteAsync("Employee/"+id.ToString()).Result;
+            TempData["SuccessMessage"] = "Deleted succesfully";
+            return RedirectToAction("Index");
+
+        }
+    }
+
+  
+}
--- WebApi/Properties/AssemblyInfo.cs
+++ WebApi/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WebApi")]
@@ -14,22 +14,22 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c5e871aa-998c-40dd-bde7-51820cc63665")]
+[assembly: Guid("bfc44628-f32e-4f47-bc7e-2013b17696c6")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
